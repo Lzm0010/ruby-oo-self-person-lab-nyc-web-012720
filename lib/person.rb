19163de108674d6b348813e0b8b35e5ab0f0097a
Lee McCormick <lzm0010@gmail.com>
2020-01-30 17:42:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness_points, :hygiene_points
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness_points = 8
+    @hygiene_points = 8
+  end
+end
