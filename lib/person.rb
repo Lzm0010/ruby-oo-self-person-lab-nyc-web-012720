fc40030d99b6fdde89ab7f0209a2d6fbe3cba4ea
Lee McCormick <lzm0010@gmail.com>
2020-01-30 18:11:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -33,13 +33,13 @@ class Person
   
   def take_bath
     self.hygiene += 4
-    "Rub-a-dub just relaxing in the tub ♫"
+    "♪ Rub-a-dub just relaxing in the tub ♫"
   end
   
   def work_out
     self.happiness += 2
     self.hygiene -= 3
-    "another one bites the dust ♫"
+    "♪ another one bites the dust ♫"
   end
 
   def call_friend(friend)
